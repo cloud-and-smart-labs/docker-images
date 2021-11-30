07f09da9f0b9cdf8e7d8f892f73e39f8432c7d18
Suvam Basak <suvambasak2008@gmail.com>
2021-11-30 18:36:49 +0530

    modify

--- LED-blink/led_blink.py
+++ LED-blink/led_blink.py
@@ -4,23 +4,29 @@ import time
 import json
 import os
 
+
 def get_delay(URL):
     with urlopen(URL) as url:
         data = json.loads(url.read().decode())
         return data['delay']
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
+
+pin = 18
 URL = 'http://'+os.environ['IP']+'/delay'
-# URL = 'http://192.168.0.222/delay'
+#URL = 'http://192.168.0.112/delay'
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(pin, GPIO.HIGH)
         time.sleep(get_delay(URL))
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(pin, GPIO.LOW)
         time.sleep(get_delay(URL))
 except Exception as e:
     print (str(e))
+    GPIO.output(pin, GPIO.LOW)
 finally:
+    GPIO.output(pin, GPIO.LOW)
     GPIO.cleanup()
