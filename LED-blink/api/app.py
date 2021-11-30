4a0fb0595884c4d5f330d7e16b058adcb4671110
Suvam Basak <suvambasak2008@gmail.com>
2021-11-30 14:18:11 +0530

    Flask api for LED dalay

---
+++ LED-blink/api/app.py
@@ -0,0 +1,20 @@
+import flask
+from flask import jsonify
+import random
+
+app = flask.Flask(__name__)
+
+@app.route('/delay', methods=['GET'])
+def delay():
+    response = {}
+    response['delay'] = random.randint(1, 10)
+    return jsonify(response)
+
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
